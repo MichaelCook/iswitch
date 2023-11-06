b654587cfbc1a07d9acefd614fe63dd026ab11ac
Michael Cook <michael@waxrat.com>
2023-11-06 10:11:29 -0500

    mc-iswitch.el: Convert tabs to spaces

--- mc-iswitch.el
+++ mc-iswitch.el
@@ -1,3 +1,5 @@
+;;  -*- lexical-binding: t; -*-
+
 ;; mc-iswitch : An isearch-like version of switch-to-buffer.
 
 ;; This software is distributed under the "Simplified BSD license":
@@ -104,10 +106,10 @@ See \\[mc-iswitch-to-buffer]."
 
 (defun mc-iswitch (prompt switch-technique)
   (let ((buf (save-window-excursion
-	       (catch 'iswitch-done
-		 (let ((bufs (apply 'append
-				    (mapcar
-				     #'(lambda (b)
+               (catch 'iswitch-done
+                 (let ((bufs (apply 'append
+                                    (mapcar
+                                     #'(lambda (b)
                                          (cond ((eq b (current-buffer))
                                                 nil)
                                                ((string-equal ""
@@ -118,156 +120,156 @@ See \\[mc-iswitch-to-buffer]."
                                                (t
                                                 (list (cons (buffer-name b)
                                                             b)))))
-				     (buffer-list))))
-		       ;(completion-ignore-case t)
-		       (name '("")))
-		   (while t
-		     (or (input-pending-p)
-			 (let (message-log-max) ; don't update *Messages*
-			   (message (concat "Switch to buffer" prompt ": %s")
-				    (mc-iswitch-prompt (car name) bufs))))
-		     (let ((event (read-event)))
-		       (cond
-			;;
-			;; DEL: remove the last character that was added to
-			;; the match string.
-			;;
-			((or (eq event ?\177)
-			     (eq event 'delete)
-			     (eq event 'backspace))
-			 (if (cdr name)
-			     (setq name (cdr name))))
-			;;
-			;; TAB: take the first character of the first buffer
-			;; name and append it to the match string.
-			;;
-			((or (eq event ?\t)
-			     (eq event 'tab))
-			 (let ((most (try-completion (car name) bufs)))
-			   (cond ((not (stringp most))
-				  (beep)) ;there are no matches
-				 ((> (length most) (length (car name)))
-				  (setq name (cons most name)))
-				 ;; Take the first character that displays
-				 ;; after the `{' and append it to the match
-				 ;; string.
-				 ((let ((first (car (all-completions
-						     (car name) bufs))))
-				    (if (< (length most) (length first))
-					(setq name (cons
-						    (concat
-						     (car name)
-						     (substring
-						      first (length most)
-						      (1+ (length most))))
-						    name))
-				      (beep)))))))
-			;;
-			;; SPC: Move the first-matched buffer name to the end
-			;; of the buffer list.
-			;;
-			((eq event ? )
-			 (let* ((a (car (all-completions
-					 (car name) bufs)))
-				(b (assq a bufs)))
-			   (setq bufs (append (delq b bufs)
-					      (list b)))))
-			;;
-			;; ?: Show a list of the buffer names that match.
-			;;
-			((eq event ??)
-			 (let ((comps (all-completions (car name) bufs)))
-			   (cond ((null comps)
-				  (message "No matches.")
-				  (sit-for 2))
-				 (t
-				  (with-output-to-temp-buffer "*Help*"
-				    (display-completion-list comps))))))
-			;;
-			;; RET: switch to the first buffer in the match list.
-			;;
-			((or (eq event ?\C-m)
-			     (eq event 'return))
-			 (let ((match (car (all-completions
-					    (car name) bufs))))
-			   (and match
-				(get-buffer match)
-				(throw 'iswitch-done (get-buffer match)))
-			   (beep)))
-			;;
-			;; C-b: set the search string to the current basename.
-			;;
-			((eq event ?\C-b)
-			 (setq name (cons (concat (car name)
-						  (file-name-nondirectory
-						   (or (buffer-file-name)
-						       (buffer-name))))
-					  name)))
-			;;
-			;; C-k: kill the first buffer in the match list.
-			;;
-			((eq event ?\C-k)
-			 (let ((match (car (all-completions
-					    (car name) bufs))))
-			   (and match
-				(kill-buffer match)
-				(setq bufs (delq (assq match bufs) bufs)))))
-			;;
-			;; C-f: take what we've typed so far (even if it does
-			;; not match any buffer names) and use it to prompt
-			;; for a file to find.  (If we've type most of a
-			;; file's name and then realize that we don't have
-			;; that file in a buffer yet, we can begin a find-file
-			;; without losing what we've typed.)
-			;;
-			((eq event ?\C-f)
-			 (throw 'iswitch-done
-				(find-file-noselect
-				 (expand-file-name
-				  (let ((n (or (car (all-completions
-						     (car name) bufs))
-					       (car name))))
-				    (read-file-name
-				     (concat "Find file" prompt ": ")
-				     nil (concat default-directory n)
-				     nil n))))))
-			;;
-			;; C-q: quote a character into the match string
-			;;
-			((eq event ?\C-q)
-			 (setq name
-			       (cons (concat
-				      (car name)
-				      (char-to-string
-				       (read-quoted-char
-					(concat "Switch to buffer" prompt
-						": " (car name) "^Q"))))
-				     name)))
-			;;
-			;; Append this character to the match string.
-			;;
-			((and (integerp event)
-			      (> event ? )
-			      (<= event ?~))
-			 (setq name (cons (concat (car name)
-						  (char-to-string event))
-					  name)))
-			;;
-			;; Push-back this event and then exit.
-			;;
-			(t
-			 (cond ((boundp 'unread-command-event)  ; LEMACS-19
-				(setq unread-command-event event))
-			       (t
-				(setq unread-command-events
-				      (cons event unread-command-events))))
-			 (throw 'iswitch-done nil))))))))))
+                                     (buffer-list))))
+                       ;(completion-ignore-case t)
+                       (name '("")))
+                   (while t
+                     (or (input-pending-p)
+                         (let (message-log-max) ; don't update *Messages*
+                           (message (concat "Switch to buffer" prompt ": %s")
+                                    (mc-iswitch-prompt (car name) bufs))))
+                     (let ((event (read-event)))
+                       (cond
+                        ;;
+                        ;; DEL: remove the last character that was added to
+                        ;; the match string.
+                        ;;
+                        ((or (eq event ?\177)
+                             (eq event 'delete)
+                             (eq event 'backspace))
+                         (if (cdr name)
+                             (setq name (cdr name))))
+                        ;;
+                        ;; TAB: take the first character of the first buffer
+                        ;; name and append it to the match string.
+                        ;;
+                        ((or (eq event ?\t)
+                             (eq event 'tab))
+                         (let ((most (try-completion (car name) bufs)))
+                           (cond ((not (stringp most))
+                                  (beep)) ;there are no matches
+                                 ((> (length most) (length (car name)))
+                                  (setq name (cons most name)))
+                                 ;; Take the first character that displays
+                                 ;; after the `{' and append it to the match
+                                 ;; string.
+                                 ((let ((first (car (all-completions
+                                                     (car name) bufs))))
+                                    (if (< (length most) (length first))
+                                        (setq name (cons
+                                                    (concat
+                                                     (car name)
+                                                     (substring
+                                                      first (length most)
+                                                      (1+ (length most))))
+                                                    name))
+                                      (beep)))))))
+                        ;;
+                        ;; SPC: Move the first-matched buffer name to the end
+                        ;; of the buffer list.
+                        ;;
+                        ((eq event ? )
+                         (let* ((a (car (all-completions
+                                         (car name) bufs)))
+                                (b (assq a bufs)))
+                           (setq bufs (append (delq b bufs)
+                                              (list b)))))
+                        ;;
+                        ;; ?: Show a list of the buffer names that match.
+                        ;;
+                        ((eq event ??)
+                         (let ((comps (all-completions (car name) bufs)))
+                           (cond ((null comps)
+                                  (message "No matches.")
+                                  (sit-for 2))
+                                 (t
+                                  (with-output-to-temp-buffer "*Help*"
+                                    (display-completion-list comps))))))
+                        ;;
+                        ;; RET: switch to the first buffer in the match list.
+                        ;;
+                        ((or (eq event ?\C-m)
+                             (eq event 'return))
+                         (let ((match (car (all-completions
+                                            (car name) bufs))))
+                           (and match
+                                (get-buffer match)
+                                (throw 'iswitch-done (get-buffer match)))
+                           (beep)))
+                        ;;
+                        ;; C-b: set the search string to the current basename.
+                        ;;
+                        ((eq event ?\C-b)
+                         (setq name (cons (concat (car name)
+                                                  (file-name-nondirectory
+                                                   (or (buffer-file-name)
+                                                       (buffer-name))))
+                                          name)))
+                        ;;
+                        ;; C-k: kill the first buffer in the match list.
+                        ;;
+                        ((eq event ?\C-k)
+                         (let ((match (car (all-completions
+                                            (car name) bufs))))
+                           (and match
+                                (kill-buffer match)
+                                (setq bufs (delq (assq match bufs) bufs)))))
+                        ;;
+                        ;; C-f: take what we've typed so far (even if it does
+                        ;; not match any buffer names) and use it to prompt
+                        ;; for a file to find.  (If we've type most of a
+                        ;; file's name and then realize that we don't have
+                        ;; that file in a buffer yet, we can begin a find-file
+                        ;; without losing what we've typed.)
+                        ;;
+                        ((eq event ?\C-f)
+                         (throw 'iswitch-done
+                                (find-file-noselect
+                                 (expand-file-name
+                                  (let ((n (or (car (all-completions
+                                                     (car name) bufs))
+                                               (car name))))
+                                    (read-file-name
+                                     (concat "Find file" prompt ": ")
+                                     nil (concat default-directory n)
+                                     nil n))))))
+                        ;;
+                        ;; C-q: quote a character into the match string
+                        ;;
+                        ((eq event ?\C-q)
+                         (setq name
+                               (cons (concat
+                                      (car name)
+                                      (char-to-string
+                                       (read-quoted-char
+                                        (concat "Switch to buffer" prompt
+                                                ": " (car name) "^Q"))))
+                                     name)))
+                        ;;
+                        ;; Append this character to the match string.
+                        ;;
+                        ((and (integerp event)
+                              (> event ? )
+                              (<= event ?~))
+                         (setq name (cons (concat (car name)
+                                                  (char-to-string event))
+                                          name)))
+                        ;;
+                        ;; Push-back this event and then exit.
+                        ;;
+                        (t
+                         (cond ((boundp 'unread-command-event)  ; LEMACS-19
+                                (setq unread-command-event event))
+                               (t
+                                (setq unread-command-events
+                                      (cons event unread-command-events))))
+                         (throw 'iswitch-done nil))))))))))
     (if buf
-	(let ((bfn (buffer-file-name buf)))
-	  (funcall switch-technique buf)
-	  (if bfn
-	      (message "File: %s" (buffer-file-name buf))
-	    (message "Buffer: %s" (buffer-name buf)))))))
+        (let ((bfn (buffer-file-name buf)))
+          (funcall switch-technique buf)
+          (if bfn
+              (message "File: %s" (buffer-file-name buf))
+            (message "Buffer: %s" (buffer-name buf)))))))
 
 (defun mc-iswitch-prompt (name bufs)
   ;;
@@ -276,32 +278,32 @@ See \\[mc-iswitch-to-buffer]."
   ;;
   (let ((comps (all-completions name bufs)))
     (cond ((null comps)
-	   (concat name " [No matches]"))
-	  ((null (cdr comps))		; One match.
-	   (concat
-	    name
-	    (and (> (length (car comps))
-		    (length name))
-		 (concat "(" (substring (car comps) (length name)) ")"))
-	    " [Matched]"))
-	  (t				; Multiple matches.
-	   (let* ((most (try-completion name bufs))
-		  (p (concat
-		      name
-		      (and (> (length most) (length name))
-			   (concat "(" (substring most (length name)) ")"))
-		      "{"
-		      (apply 'concat
-			     (cdr (apply 'append
-					 (mapcar #'(lambda (com)
+           (concat name " [No matches]"))
+          ((null (cdr comps))           ; One match.
+           (concat
+            name
+            (and (> (length (car comps))
+                    (length name))
+                 (concat "(" (substring (car comps) (length name)) ")"))
+            " [Matched]"))
+          (t                            ; Multiple matches.
+           (let* ((most (try-completion name bufs))
+                  (p (concat
+                      name
+                      (and (> (length most) (length name))
+                           (concat "(" (substring most (length name)) ")"))
+                      "{"
+                      (apply 'concat
+                             (cdr (apply 'append
+                                         (mapcar #'(lambda (com)
                                                      (list ","
                                                            (substring
                                                             com (length most))))
-						 comps))))
-		      "}"))
-		  (m (* 4 (frame-width))))
-	     (if (< (length p) m)
-		 p
-	       (concat (substring p 0 m) "...")))))))
+                                                 comps))))
+                      "}"))
+                  (m (* 4 (frame-width))))
+             (if (< (length p) m)
+                 p
+               (concat (substring p 0 m) "...")))))))
 
 ;;(mc-iswitch-prompt "abcdefg" '(("abcdefghi" . 1) ("abcdefghj" . 1) ("abcdefklm" . 2) ("abcdefnop" . 3)))
